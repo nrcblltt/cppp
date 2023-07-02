be39b324876f8486ad0c2ae4d76e388920b59898
Marco Villotta <gmarcovil@gmail.com>
2023-07-02 11:26:14 +0200

    add 06

---
+++ 06/6.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+//#include "Chapter6.h"
+
+int swap(int *x, int *y) {
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+	return 0;
+}
+
+int main() {
+	int a = 25;
+	int b = 3;
+
+	std::cout << "a: " << a << " b: " << b << std::endl;
+
+	swap(&a, &b);
+	
+	std::cout << "a: " << a << " b: " << b;
+
+	return 0;
+}
